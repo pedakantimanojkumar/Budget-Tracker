bf25d95efb19c816c0a4d9d8d61c067bca5358d3
bunny <bunny@bunnys-MacBook-Air.local>
2026-04-26 14:14:55 +0530

    Fix import path for AuthContext

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,7 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-import AuthProvider from "./context/AuthContext.jsx";
+import AuthProvider from "./context/AuthContext";
 import UIProvider from "./context/UIContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
