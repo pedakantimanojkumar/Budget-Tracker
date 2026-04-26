9842834967ad7b0f056bfc2d0e3356eaaa003eeb
bunny <bunny@bunnys-MacBook-Air.local>
2026-04-26 13:26:05 +0530

    updated API URL for production

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://budget-tracker-api.onrender.com",
 });
 
 //  THIS IS THE IMPORTANT PART
