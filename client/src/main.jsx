3a720baae22bcc73624a97808df7b8f3d2351ceb
bunny <bunny@bunnys-MacBook-Air.local>
2026-04-26 13:47:07 +0530

    fix:added AuthContext

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-import AuthProvider from "./context/AuthContext";
+import AuthProvider from "./context/AuthContext.jsx";
 import UIProvider from "./context/UIContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
