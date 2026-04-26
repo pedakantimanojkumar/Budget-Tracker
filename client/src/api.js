191e5ad261784c5f1fb73604748c2c24c7188c43
bunny <bunny@bunnys-MacBook-Air.local>
2026-04-26 22:21:24 +0530

    fix: use env for API baseURL

--- client/src/api.js
+++ client/src/api.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://budget-tracker-api.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-//  THIS IS THE IMPORTANT PART
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
